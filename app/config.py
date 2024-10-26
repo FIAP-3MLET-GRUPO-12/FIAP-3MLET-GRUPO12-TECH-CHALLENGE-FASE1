4a5d5dd679be7670a9a79b81a12767733729dcd9
Ricardo Matos <rikrdomattos@gmail.com>
2024-10-26 16:00:30 -0300

    add: env setup structure

--- app/config.py
+++ app/config.py
@@ -1,1 +1,11 @@
-# Configuração de variáveis de ambiente
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+  MONGODB_URL: str
+  MONGODB_DB: str
+  
+  class Config:
+    env_file = ".env"
+    env_file_encoding = "utf-8"
+    
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,35 +1,11 @@
 from fastapi import FastAPI
-from beanie import init_beanie, Document
-from motor.motor_asyncio import AsyncIOMotorClient
-import asyncio
+from app.config import settings
+from app.db.database import init_db
+from app.routers import user_router
 
-# Denifir um modelo Beanie
-class User(Document):
-    name: str
-    age: int
-
-    class Settings:
-        collection = "users"
-        
-# Conectar ao MongoDB e inicializar o Beanie
-async def init_db():
-    client = AsyncIOMotorClient("mongodb://localhost:27017/emprapa_db")
-    await init_beanie(database=client.embrapa_db, document_models=[User])
-        
-async def lifespan(app: FastAPI):
-    # Code before startup event
+async def lifespan(app: FastAPI) -> None:
     await init_db()
-    yield
-    # Code after startup event
     
-    
-# Criar a aplicação FastAPI
 app = FastAPI(lifespan=lifespan)
 
-    
-# Definir uma rota de exemplo
-@app.post("/users")
-async def create_user(user: User):
-    await user.insert()
-    return user
-    
+app.include_router(user_router.router, prefix="/users", tags=["users"])
