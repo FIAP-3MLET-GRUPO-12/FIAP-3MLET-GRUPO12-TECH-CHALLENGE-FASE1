ae9980ddb44d8b0fcd2be80ee6b8c49ac88d13c5
juzejunior <diogo14.shady@gmail.com>
2024-11-04 15:22:39 -0300

    feat: added user auth schema

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,8 +1,20 @@
 from beanie import Document
+from passlib.context import CryptContext
+from pydantic import EmailStr
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class User(Document):
-  name: str
-  age: int
+  email: EmailStr
+  hashed_password: str
+
+  def verify_password(self, password: str) -> bool:
+    return pwd_context.verify(password, self.hashed_password) 
+  
+  @classmethod
+  def hash_password(cls, password: str) -> str:
+    return pwd_context.hash(password)
   
   class Settings:
     collection = "users"
--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,11 +1,33 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status
+from app.auth.auth import create_access_token
 from app.models.user_model import User
+from app.schemas.token_schema import TokenSchema
 from app.schemas.user_schema import UserSchema
 
 router = APIRouter()
 
 @router.post("/", response_model=UserSchema)
 async def create_user(user: UserSchema):
-    new_user = User(name=user.name, age=user.age)
+    user_in_db = await User.find_one({"email": user.email})
+    if user_in_db:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered",
+        )
+
+    new_user = User(email=user.email, hashed_password=User.hash_password(user.password))
     await new_user.insert()
-    return new_user
+    return {"msg": "User created successfully"}
+
+@router.post("/login", response_model=TokenSchema)
+async def login(user: UserSchema):
+    db_user = await User.find_one({"email": user.email})
+    if db_user is None or not db_user.verify_password(user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Wrong Credentials", 
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(data={"sub": db_user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
+
